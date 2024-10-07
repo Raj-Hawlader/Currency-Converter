9c666224cae621b706ce47e04d980f9e74a28492
Raj-Hawlader <rajhawlader385.rh@gmail.com>
2024-10-07 23:56:33 +0600

    bangladesh,usa,china,canada_currrency

--- script.js
+++ script.js
@@ -1,32 +1,37 @@
 document.getElementById('converter-form').addEventListener('submit', function (event) {
     event.preventDefault();
+    
     const fromCurrency = document.getElementById('fromCurrency').value;
     const toCurrency = document.getElementById('toCurrency').value;
     const amount = document.getElementById('amount').value;
+    
     if (amount === '' || amount <= 0) {
         alert('Please enter a valid amount');
         return;
     }
+    
     const resultElement = document.getElementById('result');
-    resultElement.textContent = 'Fetching conversion rate';
+    resultElement.textContent = 'Fetching conversion rate...';
 
     fetch(`https://v6.exchangerate-api.com/v6/YOUR_API_KEY/latest/${fromCurrency}`)
         .then(response => response.json())
         .then(data => {
             if (data.result === "error") {
-                resultElement.textContent = 'Error fetching exchange rates. Please try again';
+                resultElement.textContent = 'Error fetching exchange rates. Please try again.';
                 return;
             }
+
             const exchangeRate = data.conversion_rates[toCurrency];
             if (!exchangeRate) {
-                resultElement.textContent ='Unable to convert to the selected currency';
+                resultElement.textContent = 'Unable to convert to the selected currency';
                 return;
             }
+
             const convertedAmount = (amount * exchangeRate).toFixed(2);
             resultElement.textContent = `${amount} ${fromCurrency} = ${convertedAmount} ${toCurrency}`;
         })
         .catch(error => {
             console.error('Error fetching exchange rates:', error);
-            resultElement.textContent ='Error fetching exchange rates. Please try again later.';
+            resultElement.textContent = 'Error fetching exchange rates. Please try again later.';
         });
 });
